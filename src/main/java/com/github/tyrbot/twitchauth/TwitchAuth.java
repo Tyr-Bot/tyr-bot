a3f8a64aab16775bf15b4fcf08644daabd910fc3
TheDrakonir <jannes@opicht.de>
2020-10-20 23:00:19 +0200

    Initial commit

---
+++ src/main/java/com/github/tyrbot/twitchauth/TwitchAuth.java
@@ -0,0 +1,34 @@
+package com.github.tyrbot.twitchauth;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.io.Resources;
+
+public class TwitchAuth {
+    
+    public final static String VERSION;
+
+    static {
+        String versionBuffer;
+        try {
+            List<String> gradleConfigLines = Resources.readLines(Resources.getResource("gradleConfig"),
+                    Charset.forName("utf-8"));
+            Map<String, String> configStore = new HashMap<>();
+            gradleConfigLines.forEach(line -> {
+                String[] values = line.split("=");
+                configStore.put(values[0], values[1]);
+            });
+
+            versionBuffer = configStore.get("version");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            versionBuffer = "UNKNOWN";
+        }
+
+        VERSION = versionBuffer;
+    }
+}
