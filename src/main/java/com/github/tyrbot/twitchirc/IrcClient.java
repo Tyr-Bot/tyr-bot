12bae700a5bb2cf3b4439e0b3e79b40fdb1c4a5f
TheDrakonir <jannes@opicht.de>
2021-04-18 16:24:32 +0200

    implement irc client

--- src/main/java/com/github/tyrbot/twitchirc/IrcClient.java
+++ src/main/java/com/github/tyrbot/twitchirc/IrcClient.java
@@ -4,28 +4,58 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
-import java.net.http.WebSocket.Listener;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+
+import com.github.tyrbot.twitchdatamodels.irc.IrcChannel;
+import com.github.tyrbot.twitchdatamodels.irc.messages.IrcMessage;
+import com.github.tyrbot.twitchdatamodels.irc.messages.control.PingMessage;
 
 public class IrcClient implements WebSocket.Listener {
+
+    private static final String PASS_COMMAND_FORMAT = "PASS oauth:%s";
+    private static final String NICK_COMMAND_FORMAT = "NICK %s";
+    private static final String JOIN_COMMAND_FORMAT = "JOIN %s";
+    private static final String PART_COMMAND_FORMAT = "PART %s";
+    private static final String PRIVMSG_COMMAND_FORMAT = "PRIVMSG %s :%s";
+    private static final String REQUIRE_CAPABILITIES_COMMAND = "CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership";
+    private static final String PONG_COMMAND = "PONG :tmi.twitch.tv";
 
     private final String token;
     private final String username;
     private final WebSocket socket;
+    private final MessagePublisher messagePublisher;
 
     private StringBuilder recievedDataStringBuffer = new StringBuilder();
 
     protected IrcClient(String token, String username, String connectionUri)
             throws URISyntaxException, InterruptedException, ExecutionException {
         this.token = token;
-        this.username = username;
+        this.username = username.toLowerCase();
         this.socket = HttpClient.newHttpClient().newWebSocketBuilder().buildAsync(new URI(connectionUri), this).get();
+        this.messagePublisher = new MessagePublisher();
+
+        connect();
     }
 
-    public CompletableFuture<Boolean> sendMessage(String message) {
-        return socket.sendText(message, true).handleAsync((ws, ex) -> {
+    public void connect() {
+        try {
+            sendCommand(String.format(PASS_COMMAND_FORMAT, token)).get();
+            sendCommand(String.format(NICK_COMMAND_FORMAT, username)).get();
+            sendCommand(REQUIRE_CAPABILITIES_COMMAND).get();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public CompletableFuture<Boolean> sendCommand(String command) {
+        //TODO: remove
+        System.out.println(">>" + command);
+        return socket.sendText(command, true).handleAsync((ws, ex) -> {
             return new CompletableFuture<>().complete(ex != null);
         });
     }
@@ -34,10 +64,21 @@ public class IrcClient implements WebSocket.Listener {
         return socket.sendClose(WebSocket.NORMAL_CLOSURE, "").thenAccept(WebSocket::abort);
     }
 
+    public CompletableFuture<Boolean> joinChannel(IrcChannel channel) {
+        return sendCommand(String.format(JOIN_COMMAND_FORMAT, channel.getChannelReference()));
+    }
+
+    public CompletableFuture<Boolean> partChannel(IrcChannel channel) {
+        return sendCommand(String.format(PART_COMMAND_FORMAT, channel.getChannelReference()));
+    }
+
+    public CompletableFuture<Boolean> sendMessage(IrcChannel channel, String message) {
+        return sendCommand(String.format(PRIVMSG_COMMAND_FORMAT, channel.getChannelReference(), message));
+    }
+
     @Override
     public void onOpen(WebSocket webSocket) {
-        // TODO Auto-generated method stub
-        Listener.super.onOpen(webSocket);
+        webSocket.request(1);
     }
 
     @Override
@@ -57,6 +98,29 @@ public class IrcClient implements WebSocket.Listener {
     }
 
     public void processMessage(String message) {
-        //TODO: Missing implementation
+        for (String splitMessage : message.split("\n")) {
+            //TODO: remove
+            System.out.println("<<" + splitMessage);
+            IrcMessage ircMessage = MessageParser.parseIrcMessage(splitMessage);
+            System.out.println(ircMessage.getClass().getSimpleName());
+            handleMessageInternal(ircMessage);
+            messagePublisher.publishMessage(ircMessage);
+        }
+    }
+
+    public void handleMessageInternal(IrcMessage message) {
+        if (message instanceof PingMessage) {
+            try {
+                sendCommand(PONG_COMMAND).get();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void registerSubscriber(Consumer<IrcMessage> subscriber, SubscriptionType... eventSubscribtions) {
+        messagePublisher.registerSubscriber(subscriber, eventSubscribtions);
     }
 }
